a0df4a2c681a1cc04c7e1470eecc0b4ae0ba09ff
maclux <tim@geohei.lu>
2024-10-03 15:39:00 +0200

    feat: add socket into parallax script

--- socketClient.py
+++ socketClient.py
@@ -1,14 +1,17 @@
 import socket
+import random
 
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(('localhost', 2000))
 
-    message = "Hello"
-    
+    x = random.uniform(-1, 1)
+    y = random.uniform(-1, 1)
+    z = random.uniform(-1, 1)
+        
+    message = f"{x},{y},{z}"
     s.sendall(message.encode('utf-8'))
     
-    # Close the socket
     s.close()
 
 if __name__ == "__main__":
