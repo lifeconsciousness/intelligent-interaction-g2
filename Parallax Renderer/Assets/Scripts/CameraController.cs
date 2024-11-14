a63e9fe7183774d68086e65da76de12d5aa56868
maclux <tim@geohei.lu>
2024-11-14 12:43:31 +0100

    feat: look at target object

--- Parallax Renderer/Assets/Scripts/CameraController.cs
+++ Parallax Renderer/Assets/Scripts/CameraController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     private Camera parallaxCamera;
-    public float parallaxFactor = 0.5f;
+    public Transform target; // The target that the camera will look at
+    public float parallaxFactor = 0.02f;
     public float smoothing = 0.1f; // Adjust this value to increase or decrease smoothing
-    public float autoResetDelay = 5f; // Time in seconds before automatic reset
+    public float autoResetDelay = 1f; // Time in seconds before automatic reset
+    public float depthMultiplier = 3f; // Adjust this value to increase or decrease depth
 
     private Vector3 initialMainCameraPos;
     private Vector3 initialParallaxCameraPos;
@@ -35,7 +37,7 @@ public class CameraController : MonoBehaviour
         if (FaceTrackerReceiver.Instance != null)
         {
             var coords = FaceTrackerReceiver.Instance.coordinates;
-            Vector3 vectorCoords = new Vector3(-coords.x, -coords.y, -coords.z);
+            Vector3 vectorCoords = new Vector3(-coords.x, -coords.y, -coords.z * depthMultiplier);
 
             // Smooth the coordinates using Lerp
             Vector3 smoothedPosition = Vector3.Lerp(lastPosition, vectorCoords, smoothing);
@@ -44,12 +46,17 @@ public class CameraController : MonoBehaviour
             // Apply the parallax effect
             parallaxCamera.transform.position = initialParallaxCameraPos + delta * parallaxFactor;
 
+            lastPosition = smoothedPosition; // Update the last position to the smoothed position
+            // Make the camera look at the target
+            if (target != null)
+            {
+                parallaxCamera.transform.LookAt(target);
+            }
+
             if (Input.GetKeyDown(KeyCode.R))
             {
                 ResetParallaxCamera();
             }
-
-            lastPosition = smoothedPosition; // Update the last position to the smoothed position
         }
     }
 
@@ -57,6 +64,11 @@ public class CameraController : MonoBehaviour
     {
         parallaxCamera.transform.position = initialParallaxCameraPos;
         initialMainCameraPos = lastPosition; // Update the initial main camera position to the last position
+
+        if (target != null)
+        {
+            parallaxCamera.transform.LookAt(target);
+        }
     }
 
     private IEnumerator AutoResetParallaxCamera()
